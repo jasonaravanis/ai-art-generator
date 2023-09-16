91fece3524b44c06697eb1c645b4f12167a78d9f
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-09-17 09:53:18 +1000

    get prompt from chat gpt

--- src/get-prompt/index.ts
+++ src/get-prompt/index.ts
@@ -1,9 +1,23 @@
+import OpenAI from 'openai';
 import getSecret from "../get-secret";
 
 const getPrompt = async (): Promise<string | null> => {
     const openAISecretKey = await getSecret('open-ai-secret-key');
 
-    return openAISecretKey
+    if (openAISecretKey === null) {
+        throw new Error("no openAI secret key found")
+    }
+
+    const openai = new OpenAI({
+        apiKey: openAISecretKey
+    })
+
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: 'Give me a prompt I can use in DALLE to generate a nice piece of art. Return nothing else.' }],
+        model: 'gpt-4'
+    })
+
+    return completion.choices[0].message.content
 }
 
 export default getPrompt
