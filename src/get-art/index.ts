df4ce3e85fd5a3cd141b69472ee3b1d36d1966e8
Jason Aravanis <64725836+jasonaravanis@users.noreply.github.com>
2023-09-17 10:25:11 +1000

    add get art utility

---
+++ src/get-art/index.ts
@@ -0,0 +1,46 @@
+import OpenAI from 'openai';
+import getSecret from "../get-secret";
+
+interface Art {
+    prompt: string;
+    image: string;
+}
+
+// const getPrompt = async (): Promise<OpenAI.Image | null> => {
+const getArt = async (): Promise<Art> => {
+    const openAISecretKey = await getSecret('open-ai-secret-key');
+
+    if (openAISecretKey === null) {
+        throw new Error("no openAI secret key found")
+    }
+
+    const openai = new OpenAI({
+        apiKey: openAISecretKey
+    })
+
+    const completion = await openai.chat.completions.create({
+        messages: [{ role: 'user', content: 'Give me a prompt I can use in DALLE to generate a nice piece of art. Return nothing else.' }],
+        model: 'gpt-4'
+    })
+
+    const prompt = completion.choices[0].message.content
+
+    if (prompt === null) {
+        throw new Error("no image generator prompt found")
+    }
+
+    // const response = await openai.images.generate({prompt, n: 1, size: '1024x1024', response_format: 'b64_json'})
+    const response = await openai.images.generate({ prompt, n: 1, size: '1024x1024', response_format: 'url' })
+
+    // const image = response.data[0].b64_json
+    const image = response.data[0].url
+
+
+    if (image === undefined) {
+        throw new Error("no image generated")
+    }
+
+    return { prompt, image }
+}
+
+export default getArt
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,13 @@
 import { type APIGatewayProxyEventV2, type APIGatewayProxyResultV2 } from 'aws-lambda'
-import getPrompt from './get-prompt'
+import getArt from './get-art'
 
-const getImage = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
-  const prompt = await getPrompt()
+const generateArt = async (event: APIGatewayProxyEventV2): Promise<APIGatewayProxyResultV2> => {
+  const art = await getArt()
 
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Hello world!',
-        prompt
-      },
-      null,
-      2
-    )
+    body: JSON.stringify(art)
   }
 }
 
-export { getImage }
+export { generateArt }
